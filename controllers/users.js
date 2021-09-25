e70833800b9803ad4542e5950e57cf037430187b
Michael-Fares <mjfares@gmail.com>
2021-09-25 08:56:20 -0500

    added users/signup and users/login routes to users contollers. Password hash with signup and jwt with login both working

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,10 @@ const mysql = require('mysql')
 const pool = require('../sql/connection')
 const { handleSQLError } = require('../sql/error')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const jwtSecret = "secret123";
+
 // get all users including decision count - working
 const getAllUsers = (req, res) => {
   // SELECT ALL USERS
@@ -13,9 +17,9 @@ const getAllUsers = (req, res) => {
   ON
   Decisions.user_id = Users.user_id
   GROUP BY
-  Users.user_id;`, (err, rows) => {
+  Users.user_id;`, (err, results) => {
     if (err) return handleSQLError(res, err)
-    return res.json(rows);
+    return res.json(results);
   })
 }
 
@@ -33,26 +37,79 @@ const getUserById = (req, res) => {
   GROUP BY
   Users.user_id`
   sql = mysql.format(sql, [req.params.user_id])
-  pool.query(sql, (err, rows) => {
+  pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
-    if(rows.length === 0) {
+    if(results.length === 0) {
       res.status(404)
     }
-    return res.json(rows);
+    return res.json(results);
   })
 }
 
-// create new user - working
-const createUser = (req, res) => {
-  const { first_name, last_name, email } = req.body
-  let sql = "INSERT INTO Users (first_name, last_name, email) VALUES (?, ?, ?);"
-  sql = mysql.format(sql, [ first_name, last_name, email ])
+//create a user - Signup user. 
+  const createUser = (req, res) => {
+  const { first_name, last_name, email, password, confirmPassword } = req.body
+  
+  if(password !== confirmPassword){
+    return res.status(400).send("Passwords do not match");
+  }
+
+  
+  let passwordHash = bcrypt.hashSync(password, 10);
+
+  let sql = "INSERT INTO Users (first_name, last_name, email, login_pwd) VALUES (?, ?, ?, ?);"
+  sql = mysql.format(sql, [ first_name, last_name, email, passwordHash ])
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
-    return res.json({ ...req.body });
+    return res.send(`New user created with id ${results.insertId}`);
   })
+  // calling next broke it, otherwise it works to insert a hashed password. So figure out how to fix it after.
 }
+
+// login user
+const loginUser = (req, res) => {
+  const { email, password } = req.body
+  let sql = "SELECT user_id, email, login_pwd FROM Users WHERE email = ?;"
+  sql = mysql.format(sql, [ email ])
+  
+  pool.query(sql, (err, results) => {
+    if (err) { 
+      return handleSQLError(res, err)
+    }
+    
+    if(results.length > 1){
+      console.error("Error, too many results with the same email" + email)
+    };
+    if(results.length == 0)(
+      console.error("Did not find a row with the email " + email)
+    )
+    if(!err && results.length == 1){
+      console.log('row password results before password hash compare: ' + results[0].login_pwd)
+      
+      let hash = results[0].login_pwd
+      
+      goodPassword = bcrypt.compareSync(password, hash)
+      console.log(`this is the result of the 'good password': ` + goodPassword)
+    }
+    
+    if(goodPassword){
+      // set the jwt id equal to the user_id that has been found in the database
+      const id = results[0].user_id
+      const unsignedToken = {
+        email: email,
+        id: id
+      }
+      const accessToken = jwt.sign(unsignedToken, jwtSecret) //string
+      res.json( { accessToken, email, id} );
+    } else{
+      res.status(401).send("Email and/or Password are incorrect")
+    }
+
+  })
+
+}
+
 
 // update feilds for user by id - working
 const updateUserById = (req, res) => {
@@ -76,7 +133,7 @@ const deleteUserById = (req, res) => {
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
-    return res.json({ message: `Deleted ${results.affectedRows} user(s) with id ${req.params.user_id}` });
+    return res.json({ message: `Deleted ${results.affectedresults} user(s) with id ${req.params.user_id}` });
   })
 }
 
@@ -84,6 +141,7 @@ module.exports = {
   getAllUsers,
   getUserById,
   createUser,
+  loginUser,
   updateUserById,
   deleteUserById
 }
--- routers/users.js
+++ routers/users.js
@@ -6,7 +6,12 @@ router.get('/', usersController.getAllUsers)
 
 router.get('/:user_id', usersController.getUserById)
 
-router.post('/', usersController.createUser)
+
+// example has these signup and login combined with one route, and called next() after signup. I split it into 2 because I can't seem to call next() without errors
+router.post('/signup', usersController.createUser)
+
+router.post('/login', usersController.loginUser)
+//
 
 router.put('/:user_id', usersController.updateUserById)
 
